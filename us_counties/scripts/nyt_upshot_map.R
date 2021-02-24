241100dfdf2dddea54b9a7474372d79bb04f79bd
tsoleary <tsoleary12@gmail.com>
2021-02-24 08:29:53 -0500

    working shiny

--- us_counties/scripts/nyt_upshot_map.R
+++ us_counties/scripts/nyt_upshot_map.R
@@ -6,20 +6,68 @@
 
 # Load packages
 require(tidyverse)
-require(urbnmapr)
+require(plotly)
 
-# Load data
-dat <- read_delim("~/Downloads/CountyData.tsv", delim = "\t")
+# Load data ---> "No Data" replaced with NA
+df <- read_delim(here::here("us_counties/data/nyt_upshot_county_data.tsv"), 
+                 delim = "\t")
+
+# Rank data
+dat <- df %>% 
+  mutate(rank_income = min_rank(desc(income)),
+         rank_education = min_rank(desc(education)),
+         rank_life = min_rank(desc(life)),
+         rank_unemployment = min_rank(unemployment),
+         rank_disability = min_rank(disability),
+         rank_unemployment = min_rank(unemployment)) %>%
+  pivot_longer(contains("rank_"), 
+               names_to = "type", 
+               values_to = "rank_n")  %>%
+  mutate(weight = case_when(type == "rank_disability" ~ 1,
+                            type == "rank_income" ~ 1,
+                            type == "rank_obesity" ~ 1,
+                            type == "rank_unemployment" ~ 1,
+                            type == "rank_education" ~ 1,
+                            type == "rank_life" ~ 1)) %>%
+  group_by(id) %>%
+  summarise(rank_avg = weighted.mean(rank_n, weight)) %>%
+  mutate(rank_me = min_rank(rank_avg))
+
+dat <- full_join(df, dat)
 
 
-# Plot an empty map of the US
-ggplot() + 
-  geom_polygon(data = urbnmapr::states, 
-               mapping = aes(x = long, y = lat, group = group),
-		           fill = “grey”, 
-		           color = “white”) +
-  coord_map(projection = "albers", 
-            lat0 = 39, 
-            lat1 = 45)
 
-# Merge state
+
+
+
+
+
+
+counties <- rjson::fromJSON(file='https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
+
+
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = col2rgb('white')
+)
+
+
+
+fig <- plot_ly() %>% 
+  add_trace(
+    type = "choropleth",
+    geojson = counties,
+    locations = str_pad(dat$id, 5, pad = "0"),
+    z = dat$rank,
+    colorscale = "Viridis",
+    zmin = 0,
+    zmax = max(dat$rank, na.rm = TRUE),
+    text = dat$County,
+    marker = list(line = list(width = 0))) %>% 
+  colorbar(title = "Rank") %>% 
+  layout(title = "Best county in US") %>% 
+  layout(geo = g)
+
+fig
--- us_counties/shiny/app.R
+++ us_counties/shiny/app.R
@@ -1,10 +1,27 @@
 library(shiny)
+library(plotly)
+library(rjson)
+library(tidyverse)
+
+df <- read_delim(url("https://tsoleary.github.io/gym_class/us_counties/data/nyt_upshot_county_data.tsv"), delim = "\t")
+
+df_long <- df %>% mutate(rank_income = min_rank(desc(income)),
+         rank_education = min_rank(desc(education)),
+         rank_life = min_rank(desc(life)),
+         rank_unemployment = min_rank(unemployment),
+         rank_disability = min_rank(disability),
+         rank_unemployment = min_rank(unemployment)) %>%
+  pivot_longer(contains("rank_"), 
+               names_to = "type", 
+               values_to = "rank_n") 
+
+counties <- rjson::fromJSON(file='https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("US Counties"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -13,43 +30,95 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
+      sliderInput(inputId = "obs_w",
+                  label = "Obesity:",
+                  min = 0,
+                  max = 5,
+                  value = 5),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "une_w",
+                  label = "Unemployment:",
+                  min = 0,
+                  max = 5,
+                  value = 5),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "dis_w",
+                  label = "Disability:",
+                  min = 0,
+                  max = 5,
+                  value = 5),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "inc_w",
+                  label = "Income:",
+                  min = 0,
+                  max = 5,
+                  value = 5),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "edu_w",
+                  label = "Education:",
+                  min = 0,
+                  max = 5,
+                  value = 5),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "lif_w",
+                  label = "Life Expectancy:",
+                  min = 0,
+                  max = 5,
+                  value = 5)
+
     ),
-    
+
     # Main panel for displaying outputs ----
     mainPanel(
       
       # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+      plotlyOutput(outputId = "distPlot")
       
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
+# Define server logic required to draw plot
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
+  output$distPlot <- renderPlotly({
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    dat <- df_long %>%
+      mutate(weight = case_when(type == "rank_disability" ~ input$dis_w,
+                                type == "rank_income" ~ input$inc_w,
+                                type == "rank_obesity" ~ input$obs_w,
+                                type == "rank_unemployment" ~ input$une_w,
+                                type == "rank_education" ~ input$edu_w,
+                                type == "rank_life" ~ input$lif_w)) %>%
+      group_by(id) %>%
+      summarise(rank_avg = weighted.mean(rank_n, weight)) %>%
+      mutate(rank_me = min_rank(rank_avg))
     
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+    dat <- full_join(df, dat)
+    
+    g <- list(
+      scope = 'usa',
+      projection = list(type = 'albers usa'),
+      showlakes = TRUE,
+      lakecolor = col2rgb('white')
+    )
+    
+    fig <- plot_ly() %>% 
+      add_trace(
+        type = "choropleth",
+        geojson = counties,
+        locations = str_pad(dat$id, 5, pad = "0"),
+        z = dat$rank_me,
+        colorscale = "Viridis",
+        zmin = 0,
+        zmax = max(dat$rank_me, na.rm = TRUE),
+        text = dat$County,
+        marker = list(line = list(width = 0))) %>% 
+      colorbar(title = "Rank") %>% 
+      layout(title = "Best county in US") %>% 
+      layout(geo = g)
+    
+    fig
     
   })
   
